b67b37812e9af050c6f8d19fe2e7e60b9f6288ab
Scott <scottobrien93@outlook.com>
2024-11-05 07:08:55 +0000

    App.jsx set up with App function ... Index.jsx functioning with correct imports ... Nav.jsx linked with nav.css for styling

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import "./nav.css";
+
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <h1>Welcome to NC News</h1>
+    </nav>
+  );
+};
+
+export default Nav;
