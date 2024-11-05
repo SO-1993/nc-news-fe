1bfecb69f2498c45d5de75d38ced0c73303fbf86
Scott <scottobrien93@outlook.com>
2024-11-05 11:16:31 +0000

    restuctured project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav/Nav";
-import ArticlesList from "./components/MainContent/ArticlesList/ArticlesList";
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/" element={<ArticlesList />} />
-      </Routes>
-    </div>
-  );
-}
+function App() {}
 
 export default App;
