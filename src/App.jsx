64504b4a3d7eda48ff529ac4c2a8f767648fddc3
Scott <scottobrien93@outlook.com>
2024-11-05 11:36:03 +0000

    Header created with 'NC News' title and Nav styled to include 'Home', 'Topics' and 'Login' links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+    </>
+  );
+}
 
 export default App;
