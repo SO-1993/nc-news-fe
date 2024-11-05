a66f1306ecf65cc395dee9150b020bc198482932
Scott <scottobrien93@outlook.com>
2024-11-05 08:44:57 +0000

    attempting to see a list of all articles (currently an error loading the articles on BE)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
+import ArticlesList from "./components/MainContent/ArticlesList/ArticlesList";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<ArticlesList />} />
+      </Routes>
     </div>
   );
 }
